eef91bdfeea46cab4db71a5c99a1ab915c536292
Manish Jaiswal <manish.jaiswal_ftc@external.swiggy.in>
2024-03-05 23:53:13 +0530

    added db connection

--- main.go
+++ main.go
@@ -1,13 +1,58 @@
 package main
 
 import (
-	"CatalogService/proto"
+	"database/sql"
 	"fmt"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
 )
+
+const (
+	dbHost     = "localhost"
+	dbPort     = "5432"
+	dbUser     = "postgres"
+	dbPassword = "Manish@2001"
+	dbName     = "Catalog"
+)
+
+type catalogService struct {
+	db *sql.DB
+}
+
+func NewCatalogService() (*catalogService, error) {
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to the database: %v", err)
+	}
+
+	err = createTables(db)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create tables: %v", err)
+	}
+
+	return &catalogService{
+		db: db,
+	}, nil
+}
+
+func createTables(db *sql.DB) error {
+	_, err := db.Exec(``)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(``)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func main() {
 	listen, err := net.Listen("tcp", ":50051")
