5f1864809f77c57cf05ad23c3e14779293bfea5b
Manish Jaiswal <manish.jaiswal_ftc@external.swiggy.in>
2024-03-06 00:36:18 +0530

    created test with test db and server

---
+++ server_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"CatalogService/proto"
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+)
+
+const (
+	testDBHost     = "localhost"
+	testDBPort     = "5432"
+	testDBUser     = "postgres"
+	testDBPassword = "Manish@2001"
+	testDBName     = "CatalogTest"
+)
+
+var (
+	testServer *grpc.Server
+	testClient proto.CatalogServiceClient
+	testDB     *sql.DB
+)
+
+func setupTest() {
+	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		testDBHost, testDBPort, testDBUser, testDBPassword, testDBName))
+	if err != nil {
+		log.Fatal(err)
+	}
+	testDB = db
+
+	err = createTables(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		listen, err := net.Listen("tcp", ":50052")
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := testServer.Serve(listen); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	conn, err := grpc.Dial("localhost:50052", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	testClient = proto.NewCatalogServiceClient(conn)
+}
+
+func tearDownTest() {
+	testServer.Stop()
+	testDB.Close()
+}
+
+func TestMain(m *testing.M) {
+	setupTest()
+	code := m.Run()
+	tearDownTest()
+	os.Exit(code)
+}
+
+func TestAddRestaurant(t *testing.T) {
+	resp, err := testClient.AddRestaurant(context.Background(), &proto.AddRestaurantRequest{Name: "Test Restaurant", Location: "Test Location"})
+	assert.NoError(t, err)
+	assert.True(t, resp.Success)
+
+}
